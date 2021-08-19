2f3195847b78c3ba0f976e2eeaa7bf7a9f7b77d5
IanChar <ianchar4@gmail.com>
2021-08-19 15:28:09 -0400

    Added early stopping to batch rl algo.

--- rlkit/core/batch_rl_algorithm.py
+++ rlkit/core/batch_rl_algorithm.py
@@ -1,6 +1,8 @@
 import abc
 
 import gtimer as gt
+import numpy as np
+
 from rlkit.core.rl_algorithm import BaseRLAlgorithm
 from rlkit.data_management.replay_buffer import ReplayBuffer
 from rlkit.samplers.data_collector import PathCollector
@@ -24,6 +26,8 @@ class BatchRLAlgorithm(BaseRLAlgorithm, metaclass=abc.ABCMeta):
             num_train_loops_per_epoch=1,
             min_num_steps_before_training=0,
             clear_buffer_every_train_loop=False,
+            early_stop_wait_epochs=None,
+            early_stop_delta=None,
     ):
         super().__init__(
             trainer,
@@ -42,6 +46,8 @@ class BatchRLAlgorithm(BaseRLAlgorithm, metaclass=abc.ABCMeta):
         self.num_expl_steps_per_train_loop = num_expl_steps_per_train_loop
         self.min_num_steps_before_training = min_num_steps_before_training
         self.clear_buffer_every_train_loop = clear_buffer_every_train_loop
+        self.early_stop_wait_epochs = early_stop_wait_epochs
+        self.early_stop_delta = early_stop_delta
 
     def _train(self):
         if self.min_num_steps_before_training > 0:
@@ -53,15 +59,26 @@ class BatchRLAlgorithm(BaseRLAlgorithm, metaclass=abc.ABCMeta):
             self.replay_buffer.add_paths(init_expl_paths)
             self.expl_data_collector.end_epoch(-1)
 
+        best_epoch = 0
+        best_return = float('-inf')
         for epoch in gt.timed_for(
                 range(self._start_epoch, self.num_epochs),
                 save_itrs=True,
         ):
-            self.eval_data_collector.collect_new_paths(
+            eval_paths = self.eval_data_collector.collect_new_paths(
                 self.max_path_length,
                 self.num_eval_steps_per_epoch,
                 discard_incomplete_paths=True,
             )
+            # Check to see if we should break
+            score = np.mean([np.sum(path['rewards']) for path in eval_paths])
+            if (self.early_stop_wait_epochs is not None
+                    and self.early_stop_delta is not None):
+                if score - best_return > self.early_stop_delta:
+                    best_epoch = epoch
+                    best_return = score
+                elif epoch - best_epoch >= self.early_stop_wait_epochs:
+                    break
             gt.stamp('evaluation sampling')
             for _ in range(self.num_train_loops_per_epoch):
                 new_expl_paths = self.expl_data_collector.collect_new_paths(
@@ -83,5 +100,5 @@ class BatchRLAlgorithm(BaseRLAlgorithm, metaclass=abc.ABCMeta):
                 self.training_mode(False)
                 if self.clear_buffer_every_train_loop:
                     self.replay_buffer.clear_buffer()
-
             self._end_epoch(epoch)
+
